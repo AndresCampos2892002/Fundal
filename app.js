6843639c853a132d2bb5ea7fa22c49a91e159eb0
AndresCampos2012 <andres.deleoncampos2002@gmail.com>
2025-09-22 00:02:30 -0600

    cambiando puerto

--- app.js
+++ app.js
@@ -193,6 +193,6 @@ app.post('/eventos', async (req, res) => {
 
 // --- INICIO DEL SERVIDOR (MODIFICADO) ---
 // Ahora usamos 'server.listen' en lugar de 'app.listen' para que socket.io funcione
-server.listen(PORT, () => {
-  console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Servidor corriendo en el puerto ${PORT}`);
 });
